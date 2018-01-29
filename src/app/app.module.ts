7d8a949b2e0e6f7609c2f98030c9310d886072f0
Lucas Martin <lumafaller@gmail.com>
2018-01-29 11:34:11 +0100

    Solucionado el problema de compilación. Duplicado de ficheros

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpModule } from '@angular/http';
 import { HttpClientModule, HttpClient } from '@angular/common/http';
