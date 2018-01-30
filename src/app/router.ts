855c5d786cb1b13444e76d39d85e577840298c90
carlos <carlosperezarandojo@gmail.com>
2018-01-30 21:56:09 +0100

    enrutado add user

--- src/app/router.ts
+++ src/app/router.ts
@@ -1,10 +1,12 @@
 import {RouterModule, Routes} from "@angular/router";
 import { PostListComponent } from "./modules/posts/post-list/post-list.component"
 import { UsersListComponent } from './modules/users/users-list/users-list.component'
+import { UsersCreateAndEditComponent } from './modules/users/users-create-and-edit/users-create-and-edit.component'
 
 const router : Routes = <Routes>[
   {path: 'posts', component: PostListComponent},
   {path: 'users', component: UsersListComponent},
+   {path: 'users/add', component: UsersCreateAndEditComponent},
   {path: '**', pathMatch: 'full', redirectTo: 'posts'}
 ];
 
